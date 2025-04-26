4057c23ef28f26cb8c5d5c259b92c7a54cb808d6
CCC200 <11540770+CCC200@users.noreply.github.com>
2025-04-25 23:16:11 -0400

    Update restore.py
    Further rework + vulpix alola line fix

--- restore.py
+++ restore.py
@@ -5,26 +5,32 @@ dir = ''
 mons = []
 mon_exceptions = ['unown', 'magikarp', 'arbok', 'pikachu', 'pichu', 'dudunsparce']
 
-def make_img(mon, mode):
-    filepath_pal = dir + mon + f'/{mode}.pal'
-    filepath_front = dir + mon + '/front.png'
-    filepath_back = dir + mon + '/back.png'
-    # exception handling
-    for exception in mon_exceptions:
-        if mon.find(exception) > -1:
-            filepath_pal = dir + exception + '/normal.pal'
-    if mon == 'gyarados': # hardcoded for red & normal gyarados forms
-        return
-    elif mon.find('gyarados') > -1:
-        filepath_back = dir + 'gyarados/back.png'
-    if not os.path.exists(filepath_pal):
-        print(f'No {mode} palette for {mon}, skipping')
-        return
-    # read palette data from normal.pal
+def make_img(mon, type, pal):
+    filepath_pal = dir + mon + f'/{pal}.pal'
+    filepath_sprite = dir + mon + f'/{type}.png'
     palette = [
         [0,0,0],
         [255,255,255]
     ]
+    # exception handling
+    for exception in mon_exceptions:
+        if mon.find(exception) > -1:
+            filepath_pal = dir + exception + f'/{pal}.pal'
+    if mon == 'gyarados': # hardcoded for red & normal gyarados forms
+        return
+    elif mon.find('gyarados') > -1 and type == 'back':
+        filepath_sprite = dir + 'gyarados/back.png'
+    elif mon == 'ninetales_alolan' or mon == 'vulpix_alolan':
+        if type == 'back':
+            # hardcoded bw swap for vulpix alola line
+            palette = [
+                [255,255,255],
+                [0,0,0],
+            ]
+    if not os.path.exists(filepath_pal):
+        print(f'No {pal} palette for {mon}, skipping')
+        return
+    # read palette data from normal.pal
     pal_file = open(filepath_pal, 'r')
     content = pal_file.read()
     pal_file.close()
@@ -40,17 +46,12 @@ def make_img(mon, mode):
                     rgb_val.append(x)
                 palette.insert(1, rgb_val)
     palette = [value for color in palette for value in color]
-    # build sprites
-    if os.path.exists(filepath_back):
-        back = Image.open(filepath_back)
+    # build sprite
+    if os.path.exists(filepath_sprite):
+        back = Image.open(filepath_sprite)
         back = back.convert('P')
         back.putpalette(palette)
-        back.save(f'sprites/back{'-shiny' if mode == 'shiny' else ''}/{mon}.png')
-    if os.path.exists(filepath_front):
-        front = Image.open(filepath_front)
-        front = front.convert('P')
-        front.putpalette(palette)
-        front.save(f'sprites/front{'-shiny' if mode == 'shiny' else ''}/{mon}.png')
+        back.save(f'sprites/{type}{'-shiny' if pal == 'shiny' else ''}/{mon}.png')
 # main
 if len(sys.argv) < 2:
 	print('Point to gfx/pokemon dir')
@@ -64,5 +65,7 @@ if not os.path.isdir('sprites'):
     os.mkdir('sprites/back')
     os.mkdir('sprites/back-shiny')
 for mon in mons:
-    make_img(mon, 'normal')
-    make_img(mon, 'shiny')
+    make_img(mon, 'front', 'normal')
+    make_img(mon, 'front', 'shiny')
+    make_img(mon, 'back', 'normal')
+    make_img(mon, 'back', 'shiny')
